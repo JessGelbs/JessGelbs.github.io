ad8219c29d9d453bd252aebd6ce26e84d37a0cc1
Jessica Gelbart <jessica.c.gelbart@gmail.com>
2018-12-03 18:44:22 -0500

    Classwork

---
+++ GA_Workshop/ga_press_release/script.js
@@ -0,0 +1,8 @@
+console.log( 'hello, world!' )
+
+document.querySelector('h1').style.color= 'blue'
+
+document.querySelector('#ga-text').style.color= 'blue'
+
+document.querySelector('list').style.font_family= 'cursive'
+
