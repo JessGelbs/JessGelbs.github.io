c36b278a7c05c280e4888e2cc546c214bc4aee1a
Jessica Gelbart <jessica.c.gelbart@gmail.com>
2018-12-10 20:46:29 -0500

    Classwork

---
+++ Class 07/first-javascript/script.js
@@ -0,0 +1,24 @@
+let product = 10 * 10;
+
+console.log(product);
+
+let quotient = 50/17;
+
+console.log(quotient);
+
+let difference = 50-17
+
+console.log(difference);
+
+let sum = 50+17
+
+console.log(sum);
+
+let first = 'Wade';
+let last = 'Wilson';
+
+console.log(first + last)
+
+// alternative new way!! es6 string templates
+
+console.log(`Hello my name is ${first} ${last}`)
