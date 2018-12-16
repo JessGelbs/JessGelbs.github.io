79bba88830735498958b3a82ffaa1a20afe55edb
Jessica Gelbart <jessica.c.gelbart@gmail.com>
2018-12-15 19:41:54 -0500

    RGB Color Lab/ Homework

--- Class 08/RGB Color Choice/js/project.js
+++ Class 08/RGB Color Choice/js/project.js
@@ -12,21 +12,19 @@
 
 // 5. Use .innerHTML to change the text inside of #colorful-text to colorStr
 
-
 // 6. Use .style.background to change the background of #wrapper to colorStr
 
-document.querySelector( "#color-button" ).onclick = changeColor
+document.querySelector("#color-button").onclick = changeColor;
 
-function changeColor () {
-	var red = document.querySelector( "#red" ).value
-	var green = document.querySelector( "#green" ).value
-	var blue = document.querySelector( "#blue" ).value
+function changeColor() {
+  var red = document.querySelector("#red").value;
+  var green = document.querySelector("#green").value;
+  var blue = document.querySelector("#blue").value;
 
-	var colorStr = 'rgb(' + red + ' , ' + green + ' , ' + blue + ' )'
-	console.log(colorStr)
+  var colorStr = "rgb(" + red + " , " + green + " , " + blue + " )";
+  console.log(colorStr);
 
-	document.querySelector('#colorful-text').innerHTML = colorStr
+  document.querySelector("#colorful-text").innerHTML = colorStr;
 
-	document.querySelector('#wrapper').style.background = colorStr
-
+  document.querySelector("#wrapper").style.background = colorStr;
 }
