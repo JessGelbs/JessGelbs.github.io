df16d72fb8c94059e88a39831124e2bab7d2ddbf
Jessica Gelbart <jessica.c.gelbart@gmail.com>
2018-12-15 23:05:56 -0500

    Lifetime Supply

---
+++ Class 08/Lifetime Supply/js/project.js
@@ -0,0 +1,16 @@
+
+document.querySelector('#click-me').onclick = solution;
+
+let age = '#age'
+let maxAge = '#max-age'
+let favoriteDrink = '#item'
+let numPerDay = '#num-per-day'
+
+function solution() {
+    
+    let drinksPerYear = favoriteDrink * 365;
+    let yearsLeft = maxAge - age;
+    let totalDrinks = drinksPerYear * yearsLeft;
+
+    var calculate = document.querySelector()
+}
