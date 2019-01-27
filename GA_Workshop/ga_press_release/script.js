85e8ba3b5a2de0dfff99882f082acc8c2e7a7d65
JessicaGelbart <jessica.c.gelbart@gmail.com>
2019-01-27 17:47:46 -0500

    Computer Malfunction

--- GA_Workshop/ga_press_release/script.js
+++
@@ -1,8 +0,0 @@
-console.log( 'hello, world!' )
-
-document.querySelector('h1').style.color= 'blue'
-
-document.querySelector('#ga-text').style.color= 'blue'
-
-document.querySelector('list').style.font_family= 'cursive'
-
