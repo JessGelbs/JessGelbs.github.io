aa716f58ddea4119066a671dae5ee303329957a6
JessicaGelbart <jessica.c.gelbart@gmail.com>
2019-01-28 18:38:13 -0500

    edits

---
+++ Class 17/carousel/js/index.js
@@ -0,0 +1,5 @@
+let images = ['images/food2.jpg', 'images/food3.jpg', 'images/food4.jpg', 'images/food5.jpg', 'images/food6.jpg', 'images/food7.jpg'];
+let imageIndex = 0;
+
+$('.btn-next').click(nextImage);
+$('.btn-prev').click(prevImage);
