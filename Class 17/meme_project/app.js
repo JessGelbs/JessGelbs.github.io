95eb6810f7443a3d1da22a5608f6bb8b26990120
JessicaGelbart <jessica.c.gelbart@gmail.com>
2019-01-28 21:28:30 -0500

    classwork

--- Class 17/meme_project/app.js
+++ Class 17/meme_project/app.js
@@ -1,17 +1,31 @@
 // Create a submit handler for the meme form
 
+$(".meme-form").submit(function() {
+  event.preventDefault();
+
+  let memeInput = $(".meme-input").val();
+
+  var xhr = $.get(
+    "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=TtGNnu1sbhfA0ur1ICuudu3A3s9OifJO&limit`=5");
+  xhr.done(function(data) {
+    console.log("success got data", data);
+  });
+
+  console.log(memeInput);
+});
+
 // Do all of the following inside of this submit handler function...
 
-  // Stop the page from refreshing when the form is submitted  (event.preventDefault())
+// Stop the page from refreshing when the form is submitted  (event.preventDefault())
 
-  // Get the value of meme-input and save as a variable
+// Get the value of meme-input and save as a variable
 
-  // Use the JavaScript example at https://developers.giphy.com/docs/ to get the
-  // results from the http://api.giphy.com/v1/gifs/search endpoint (exactly as in example) but changing in your API key (you will need to register for API key)
+// Use the JavaScript example at https://developers.giphy.com/docs/ to get the
+// results from the http://api.giphy.com/v1/gifs/search endpoint (exactly as in example) but changing in your API key (you will need to register for API key)
 
-  // Use your knowledge of objects and arrays to get the list of give memes
+// Use your knowledge of objects and arrays to get the list of give memes
 
-  // Swap out Ryan Goesling for your input term using string concatenation
+// Swap out Ryan Goesling for your input term using string concatenation
 
-  // Use a forEach loop to append a list element containing a video to meme-list for each meme video
-  // hint: <li><video src='+ looping gif image path here + ' type="video/mp4" autoplay></video></li>
+// Use a forEach loop to append a list element containing a video to meme-list for each meme video
+// hint: <li><video src='+ looping gif image path here + ' type="video/mp4" autoplay></video></li>
