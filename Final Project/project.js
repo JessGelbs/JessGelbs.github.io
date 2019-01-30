5468c6de10e749a8539874196718d912401e36e7
JessicaGelbart <jessica.c.gelbart@gmail.com>
2019-01-30 18:36:46 -0500

    edits

--- Final Project/project.js
+++ Final Project/project.js
@@ -2,3 +2,7 @@ $(document).ready (function(){
     $('h3').fadeToggle(3000);
 });
 
+$('.tattoo_pic').click(function(){
+    const src = $(this).attr('src');
+    $('#bigimage').attr('src', src);
+});
